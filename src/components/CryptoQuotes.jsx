c10f00e49b7412dea7fa62b4aa7076a243c7a992
Alex Beltz <beltz.a@outlook.com>
2023-03-27 17:09:47 +0800

    Remove from active until a fix is found

--- src/components/CryptoQuotes.jsx
+++ src/components/CryptoQuotes.jsx
@@ -1,5 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function CryptoQuotes() {
-  return <div>CryptoQuotes</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        "/api/v1/cryptocurrency/listings/latest",
+        {
+          params: {
+            start: 1,
+            limit: 20,
+            convert: "USD",
+            sort: "market_cap",
+            sort_dir: "desc",
+            cryptocurrency_type: "coins",
+            tag: "privacy",
+            CMC_PRO_API_KEY: "<Your_API_Key>",
+          },
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      );
+      setData(response.data.data);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        margin: "5rem 0 0 5rem",
+        width: "60%",
+      }}
+    >
+      {data.map((crypto) => (
+        <div
+          key={crypto.id}
+          style={{
+            width: "300px",
+            height: "300px",
+            margin: "1rem",
+            backgroundColor: "#f0f0f0",
+            borderRadius: "1rem",
+            padding: "1rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            <h3>{crypto.name}</h3>
+            <p>Symbol: {crypto.symbol}</p>
+            <p>Market Cap: {crypto.quote.USD.market_cap}</p>
+          </div>
+          <div>
+            <p>Price: {crypto.quote.USD.price}</p>
+            <p>Volume (24h): {crypto.quote.USD.volume_24h}</p>
+            <p>Change (24h): {crypto.quote.USD.percent_change_24h}%</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/components/PdfToVoiceTool.jsx
+++ src/components/PdfToVoiceTool.jsx
@@ -1,329 +1,112 @@
-// import React, { useState } from "react";
-// import "../styles/App.css";
-// import "../styles/PdfToVoiceTool.css";
-// // import pdfjs from "pdfjs-dist/build/pdf";
-// import pdfjsLib from "pdfjs-dist/legacy/build/pdf";
-// import { saveAs } from "file-saver";
-// import responsiveVoice from "responsivevoice";
+import React, { useState } from "react";
+import "../styles/App.css";
+import "../styles/PdfToVoiceTool.css";
+import { saveAs } from "file-saver";
+import { useSpeechSynthesis } from "react-speech-kit";
+import { getDocument } from "pdfjs-dist";
 
-// export default function PdfToVoiceTool() {
-//   const [audioData, setAudioData] = useState(null);
-//   const [progress, setProgress] = useState(0);
+export default function PdfToVoiceTool() {
+  const [audioData, setAudioData] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [language, setLanguage] = useState("");
+  const { speak, voices } = useSpeechSynthesis();
 
-//   const handleFileDrop = async (event) => {
-//     event.preventDefault();
+  const handleFileDrop = async (event) => {
+    event.preventDefault();
 
-//     const file = event.dataTransfer.files[0];
+    if (!language) {
+      alert("Please choose a language");
+      return;
+    }
 
-//     console.log(file);
+    const file = event.dataTransfer.files[0];
 
-//     if (!file.type.startsWith("application/pdf")) {
-//       alert("Please drop a PDF file");
-//       return;
-//     }
+    console.log(file);
 
-//     const pdfData = await file.arrayBuffer();
-//     // const pdf = await pdfjs.getDocument({ data: pdfData }).promise;
-//     const pdf = await pdfjsLib.getDocument({ data: pdfData }).promise;
-//     const numPages = pdf.numPages;
+    if (!file.type.startsWith("application/pdf")) {
+      alert("Please drop a PDF file");
+      return;
+    }
 
-//     let text = "";
-//     for (let i = 1; i <= numPages; i++) {
-//       const page = await pdf.getPage(i);
-//       const pageText = await page.getTextContent();
-//       text += pageText.items.map((item) => item.str).join(" ");
-//       setProgress((i / numPages) * 100); // Update progress percentage
-//     }
+    const pdfData = await file.arrayBuffer();
+    const pdf = await getDocument({ data: pdfData }).promise;
+    const numPages = pdf.numPages;
 
-//     const audioBlob = await textToSpeech(text);
-//     setAudioData(audioBlob);
-//     setProgress(0); // Reset progress percentage
-//   };
+    let text = "";
+    for (let i = 1; i <= numPages; i++) {
+      const page = await pdf.getPage(i);
+      const pageText = await page.getTextContent();
+      text += pageText.items.map((item) => item.str).join(" ");
+      setProgress(((i + 1) / numPages) * 100); // Update progress percentage
+    }
 
-//   const handleDownload = () => {
-//     if (audioData) {
-//       const url = URL.createObjectURL(audioData);
-//       const link = document.createElement("a");
-//       link.href = url;
-//       link.download = "audio.mp3";
-//       document.body.appendChild(link);
-//       link.click();
-//       document.body.removeChild(link);
-//     }
-//   };
+    const audioBlob = await textToSpeech(text);
+    setAudioData(audioBlob);
+    setProgress(0); // Reset progress percentage
+  };
 
-//   const textToSpeech = async (text) => {
-//     return new Promise((resolve, reject) => {
-//       responsiveVoice.speak(text, "UK English Female", {
-//         onend: () => {
-//           const audioBlob = new Blob([responsiveVoice.voice.wav], {
-//             type: "audio/wav",
-//           });
-//           resolve(audioBlob);
-//         },
-//         onerror: () => {
-//           reject(new Error("Text-to-speech conversion failed"));
-//         },
-//         onprogress: (percent) => {
-//           setProgress(percent); // Update progress percentage
-//         },
-//       });
-//     });
-//   };
+  const handleDownload = () => {
+    if (audioData) {
+      saveAs(audioData, "audio.mp3");
+    }
+  };
 
-//   return (
-//     <div
-//       className="pdfDropBox shadow"
-//       onDrop={handleFileDrop}
-//       onDragOver={(event) => event.preventDefault()}
-//     >
-//       {audioData ? (
-//         <>
-//           <p className="pdfDropBoxText">Conversion complete</p>
-//           <button onClick={handleDownload}>Download audio</button>
-//         </>
-//       ) : (
-//         <>
-//           <p className="pdfDropBoxText">
-//             Drag a PDF file here to convert to voice
-//           </p>
-//           <div className="progress-bar">
-//             <div className="progress" style={{ width: `${progress}%` }}></div>
-//           </div>
-//           <p className="pdfDropBoxText">{Math.round(progress)}% complete</p>
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-// import React, { useState } from "react";
-// import "../styles/App.css";
-// import "../styles/PdfToVoiceTool.css";
-// import { PDFDoc, PDFText } from "@foxitsoftware/foxit-pdf-sdk-for-web";
-// import { saveAs } from "file-saver";
-// import responsiveVoice from "responsivevoice";
+  const textToSpeech = async (text) => {
+    return new Promise((resolve) => {
+      const selectedVoice = voices.find((voice) => voice.lang === language);
+      speak({
+        text,
+        voice: selectedVoice,
+        onEnd: () => {
+          resolve();
+        },
+      });
+    });
+  };
 
-// export default function PdfToVoiceTool() {
-//   const [audioData, setAudioData] = useState(null);
-//   const [progress, setProgress] = useState(0);
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
+  };
 
-//   const handleFileDrop = async (event) => {
-//     event.preventDefault();
-
-//     const file = event.dataTransfer.files[0];
-
-//     console.log(file);
-
-//     if (!file.type.startsWith("application/pdf")) {
-//       alert("Please drop a PDF file");
-//       return;
-//     }
-
-//     const pdfData = await file.arrayBuffer();
-//     const pdf = await PDFDoc.load(pdfData);
-//     const numPages = pdf.getPageCount();
-
-//     let text = "";
-//     for (let i = 0; i < numPages; i++) {
-//       const page = await pdf.getPage(i);
-//       const pageText = await PDFText.getText(page);
-//       text += pageText.replace(/\s+/g, " ");
-//       setProgress(((i + 1) / numPages) * 100); // Update progress percentage
-//     }
-
-//     const audioBlob = await textToSpeech(text);
-//     setAudioData(audioBlob);
-//     setProgress(0); // Reset progress percentage
-//   };
-
-//   const handleDownload = () => {
-//     if (audioData) {
-//       const url = URL.createObjectURL(audioData);
-//       const link = document.createElement("a");
-//       link.href = url;
-//       link.download = "audio.mp3";
-//       document.body.appendChild(link);
-//       link.click();
-//       document.body.removeChild(link);
-//     }
-//   };
-
-//   const textToSpeech = async (text) => {
-//     return new Promise((resolve, reject) => {
-//       responsiveVoice.speak(text, "UK English Female", {
-//         onend: () => {
-//           const audioBlob = new Blob([responsiveVoice.voice.wav], {
-//             type: "audio/wav",
-//           });
-//           resolve(audioBlob);
-//         },
-//         onerror: () => {
-//           reject(new Error("Text-to-speech conversion failed"));
-//         },
-//         onprogress: (percent) => {
-//           setProgress(percent); // Update progress percentage
-//         },
-//       });
-//     });
-//   };
-
-//   return (
-//     <div
-//       className="pdfDropBox shadow"
-//       onDrop={handleFileDrop}
-//       onDragOver={(event) => event.preventDefault()}
-//     >
-//       {audioData ? (
-//         <>
-//           <p className="pdfDropBoxText">Conversion complete</p>
-//           <button onClick={handleDownload}>Download audio</button>
-//         </>
-//       ) : (
-//         <>
-//           <p className="pdfDropBoxText">
-//             Drag a PDF file here to convert to voice
-//           </p>
-//           <div className="progress-bar">
-//             <div className="progress" style={{ width: `${progress}%` }}></div>
-//           </div>
-//           <p className="pdfDropBoxText">{Math.round(progress)}% complete</p>
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-// import React, { useState } from "react";
-// import "../styles/App.css";
-// import "../styles/PdfToVoiceTool.css";
-// import { PDFDoc, PDFText } from "@foxitsoftware/foxit-pdf-sdk-for-web";
-// import { saveAs } from "file-saver";
-// import responsiveVoice from "responsivevoice";
-
-// export default function PdfToVoiceTool() {
-//   const [audioData, setAudioData] = useState(null);
-//   const [progress, setProgress] = useState(0);
-//   const [language, setLanguage] = useState("");
-
-//   const handleFileDrop = async (event) => {
-//     event.preventDefault();
-
-//     if (!language) {
-//       alert("Please choose a language");
-//       return;
-//     }
-
-//     const file = event.dataTransfer.files[0];
-
-//     console.log(file);
-
-//     if (!file.type.startsWith("application/pdf")) {
-//       alert("Please drop a PDF file");
-//       return;
-//     }
-
-//     const pdfData = await file.arrayBuffer();
-//     const pdf = await PDFDoc.load(pdfData);
-//     const numPages = pdf.getPageCount();
-
-//     let text = "";
-//     for (let i = 0; i < numPages; i++) {
-//       const page = await pdf.getPage(i);
-//       const pageText = await PDFText.getText(page);
-//       text += pageText.replace(/\s+/g, " ");
-//       setProgress(((i + 1) / numPages) * 100); // Update progress percentage
-//     }
-
-//     const audioBlob = await textToSpeech(text);
-//     setAudioData(audioBlob);
-//     setProgress(0); // Reset progress percentage
-//   };
-
-//   const handleDownload = () => {
-//     if (audioData) {
-//       const url = URL.createObjectURL(audioData);
-//       const link = document.createElement("a");
-//       link.href = url;
-//       link.download = "audio.mp3";
-//       document.body.appendChild(link);
-//       link.click();
-//       document.body.removeChild(link);
-//     }
-//   };
-
-//   const textToSpeech = async (text) => {
-//     return new Promise((resolve, reject) => {
-//       responsiveVoice.speak(text, language, {
-//         onend: () => {
-//           const audioBlob = new Blob([responsiveVoice.voice.wav], {
-//             type: "audio/wav",
-//           });
-//           resolve(audioBlob);
-//         },
-//         onerror: () => {
-//           reject(new Error("Text-to-speech conversion failed"));
-//         },
-//         onprogress: (percent) => {
-//           setProgress(percent); // Update progress percentage
-//         },
-//       });
-//     });
-//   };
-
-//   return (
-//     <div className="pdfToVoiceToolContainer">
-//       <div className="languageSelect">
-//         <select style={{ padding: "1.5rem", fontSize: "1.25rem" }}>
-//           <option value="" disabled selected>
-//             Please choose a language
-//           </option>
-//           <option value="en-US">English (US)</option>
-//           <option value="en-GB">English (UK)</option>
-//           <option value="es-ES">Español (España)</option>
-//           <option value="fr-FR">Français (France)</option>
-//           <option value="de-DE">Deutsch (Deutschland)</option>
-//         </select>
-//       </div>
-//       <div
-//         className="pdfDropBox shadow"
-//         onDrop={handleFileDrop}
-//         onDragOver={(event) => event.preventDefault()}
-//       >
-//         {audioData ? (
-//           <>
-//             <p className="pdfDropBoxText">Conversion complete</p>
-//             <button onClick={handleDownload}>Download audio</button>
-//           </>
-//         ) : (
-//           <>
-//             <p className="pdfDropBoxText">
-//               Drag a PDF file here to convert to voice
-//             </p>
-//             <div className="progress-bar">
-//               <div className="progress" style={{ width: `${progress}%` }}></div>
-//             </div>
-//             <p className="pdfDropBoxText">{Math.round(progress)}% complete</p>
-//           </>
-//         )}
-//         {!window.FoxitPDFSDK ? (
-//           <p className="pdfDropBoxText">Failed to load PDF SDK</p>
-//         ) : null}
-//       </div>
-//     </div>
-//   );
-// }
+  return (
+    <div className="pdfToVoiceToolContainer">
+      <div className="languageSelect">
+        <select style={{ padding: "1.5rem", fontSize: "1.25rem" }}>
+          <option value="" disabled selected>
+            Please choose a language
+          </option>
+          <option value="en-US">English (US)</option>
+          <option value="en-GB">English (UK)</option>
+          <option value="es-ES">Español (España)</option>
+          <option value="fr-FR">Français (France)</option>
+          <option value="de-DE">Deutsch (Deutschland)</option>
+        </select>
+      </div>
+      <div
+        className="pdfDropBox shadow"
+        onDrop={handleFileDrop}
+        onDragOver={(event) => event.preventDefault()}
+      >
+        {audioData ? (
+          <>
+            <p className="pdfDropBoxText">Conversion complete</p>
+            <button onClick={handleDownload}>Download audio</button>
+          </>
+        ) : (
+          <>
+            <p className="pdfDropBoxText">
+              Drag a PDF file here to convert to voice
+            </p>
+            <div className="progress-bar">
+              <div className="progress" style={{ width: `${progress}%` }}></div>
+            </div>
+            <p className="pdfDropBoxText">{Math.round(progress)}% complete</p>
+          </>
+        )}
+        {!window.FoxitPDFSDK ? (
+          <p className="pdfDropBoxText">Failed to load PDF SDK</p>
+        ) : null}
+      </div>
+    </div>
+  );
+}
